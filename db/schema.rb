b67e7e850286a8ab3c1c7bc2eefe2b5484982c21
junebug5543 <junebug5543@gmail.com>
2015-09-16 08:55:17 -0500

    Added fields to Users

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150915204234) do
+ActiveRecord::Schema.define(version: 20150915232804) do
 
   create_table "restaurants", force: :cascade do |t|
     t.string   "name"
@@ -45,6 +45,9 @@ ActiveRecord::Schema.define(version: 20150915204234) do
     t.string   "last_sign_in_ip"
     t.datetime "created_at",                          null: false
     t.datetime "updated_at",                          null: false
+    t.boolean  "admin"
+    t.string   "first_name"
+    t.string   "last_name"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
